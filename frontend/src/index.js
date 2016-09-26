1e253475f5991767771c835c85d7818c37f77394
Andrew Sheldon <asheldon55@gmail.com>
2016-09-26 15:58:18 +1000

    Add missing semi-colons from import statements.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 
-import baseReducer from './reducers'
-import routes from './routes'
+import baseReducer from './reducers';
+import routes from './routes';
 
 import './css/index.css';
 
