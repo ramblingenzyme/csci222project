3ad6e56090eb5e1d744048ba4006b01244d10b55
Brett Latham <lathobravo957@rocketmail.com>
2016-09-28 14:55:55 +1000

    First mock up. Working:stubs for sidebars, Menu. Needs work: css autoadjust, login

---
+++ frontend/src/components/SidebarAnalysis.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import '../css/App.css';
+
+// This is the sidebar for analysis
+class SidebarSortBugs extends Component { 
+
+    render() {
+        return (
+            <div className="Sidebar">
+                Analysis
+            </div>
+        );
+    }
+}
+
+export default SidebarSortBugs;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,16 +1,66 @@
 import App from '../components/App.js';
 import Login from '../components/Login.js';
+import SidebarSearch from '../components/SidebarSearch.js';
+import SidebarAnalysis from '../components/SidebarAnalysis.js';
+import SidebarAssign from '../components/SidebarAssign.js';
 
 export default {
     path: '/',
     component: App,
     indexRoute: {
-        component: Login
+        components: {
+            main: Login,
+            sidebar: SidebarSearch
+        }
     },
     childRoutes: [
         {
             path: 'test/:username',
-            component: Login
+            components: {
+                main: Login,
+                sidebar: SidebarSearch
+            }
+        },
+        {
+            path: '/Report/',
+            component: {
+                main: Login,
+            }
+        },
+        { 
+            path: '/Analysis/',
+            components: {
+                main: Login,
+                sidebar: SidebarAnalysis
+            }
+        },
+        {
+            path: '/Assign/',
+            components: {
+                main: Login,
+                sidebar: SidebarAssign
+            }
+        },
+        {
+            path: '/Review/',
+            component: {
+                main: Login,
+            }
+        },
+        {
+            path: '/Search/',
+            components: {
+                main: Login,
+                sidebar: SidebarSearch
+            }
+        },
+        {
+            path: '/Profile/',
+            component: {
+                main: Login,
+            } 
         }
+         
+        
     ]
 }
