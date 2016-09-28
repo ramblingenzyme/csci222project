bf43f19983139dd9e0ebf54e3e23b13079638ed0
Geniusflip <ryanherford@gmail.com>
2016-09-28 17:39:27 +1000

    Add files via upload
    Updated entities and controllers. All user entities have been implemented, controllers are WIP

---
+++ backend/backend/src/controller.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+#include "entities.h"
+
+class ReporterController{
+	private:
+		Reporter* user; 
+	public:
+		Reporter* get_user();
+		void set_user(Reporter*);
+}
+
+#endif
