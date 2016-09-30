d968a8e2c97b5a30a22588cd6bc7ed847c2414d0
kranzuft <kranzuft@users.noreply.github.com>
2016-09-30 18:05:17 +1000

    Added component for profile page, currently shows basic data

---
+++ frontend/src/components/ProfilePage.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+
+class ProfilePage extends Component {
+    render() {
+        let content = this._getContent();
+        return (
+            <div>
+                {content}
+            </div>
+        )
+    }
+
+    _getContent() {
+        if (this.props.loggedIn) {
+            return (
+                <section className="ProfileInfo">
+                    <div id="username">
+                        <strong>Username:</strong>
+                        <span>{this.props.username}</span>
+                    </div>
+                    <div id="password">
+                        <strong>Password:</strong>
+                        <span>{this.props.password}</span>
+                    </div>
+                    <div id="email">
+                        <strong>Email:</strong>
+                        <span>{this.props.email}</span>
+                    </div>
+                </section>
+            )
+        } else {
+            return (
+                <p>
+                    You must <Link to="/login">login</Link> to view this page
+                </p>
+            )
+        }
+    }
+}
+
+export default ProfilePage;
