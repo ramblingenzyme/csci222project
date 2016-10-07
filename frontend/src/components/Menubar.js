0f4d7de3999071f0936359678f67d0ac8edad898
kranzuft <kranzuft@users.noreply.github.com>
2016-10-08 03:44:45 +1100

    Modified menubar css to be slightly more aui complient; Fixed css

--- frontend/src/components/Menubar.js
+++ frontend/src/components/Menubar.js
@@ -4,26 +4,30 @@ import { Link } from 'react-router';
 class Menubar extends Component {
     render() {
         return (
-            <ul className="aui-nav">
-                <li>
-                    <Link to={'/Report/'}>Report </Link>
-                </li>
-                <li>
-                    <Link to={'/Analysis/'}>Analysis </Link>
-                </li>
-                <li>
-                    <Link to={'/Assign/'}>Assign </Link>
-                </li>
-                <li>
-                    <Link to={'/Review/'}>Review </Link>
-                </li>
-                <li>
-                    <Link to={'/Search/'}>Search </Link>
-                </li>
-                <li>
-                    <Link to={'/Profile/'}>Profile </Link>
-                </li>
-            </ul>
+            <div className="aui-primary-header">
+                <h1 id="logo" className="aui-header-logo aui-header-logo-aui">
+                </h1>
+                <ul className="aui-nav">
+                    <li>
+                        <Link to={'/Report/'}>Report </Link>
+                    </li>
+                    <li>
+                        <Link to={'/Analysis/'}>Analysis </Link>
+                    </li>
+                    <li>
+                        <Link to={'/Assign/'}>Assign </Link>
+                    </li>
+                    <li>
+                        <Link to={'/Review/'}>Review </Link>
+                    </li>
+                    <li>
+                        <Link to={'/Search/'}>Search </Link>
+                    </li>
+                    <li>
+                        <Link to={'/Profile/'}>Profile </Link>
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
