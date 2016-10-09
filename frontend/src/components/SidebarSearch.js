3d1633c7089bbab39991be8ff8cd711824296c98
kranzuft <kranzuft@users.noreply.github.com>
2016-10-09 23:30:30 +1100

    Finished the sidebar selectors for the bug search. This took far too long. Curse you imported CSS!

--- frontend/src/components/SidebarSearch.js
+++ frontend/src/components/SidebarSearch.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import Search from './Search.js'
+import { Field, reduxForm } from 'redux-form';
+import DropdownSelector from './AUIDropdown.js'
 
 import '../css/App.css';
 
 // This is the default sidebar; for sort bugs
 class SidebarSearch extends Component {
-
     render() {
+        var timeline_options = [
+            "Day",
+            "Week",
+            "Month",
+            "Year"
+        ];
+        var status_options = [
+            "Unassigned",
+            "Assigned",
+            "Patch Submitted",
+            "Resolved"
+        ];
+        var magnitudal_options = [
+            "Low",
+            "Medium",
+            "High",
+            "Extreme"
+        ];
         return (
             <div className="aui-page-panel-nav">
-                <nav className="aui-navgroup aui-navgroup-vertical">
-                    <div className="aui-navgroup-inner">
-                        <div className="aui-sidebar-group aui-sidebar-group-tier-one">
-                            <div className="aui-nav-heading" title="bug-page-search">
-                                <strong>Search Bugs In Project</strong>
-                            </div>
-                            <ul className="aui-nav" title="Page actions">
-                                <li>
-                                    <a className="aui-nav-item" href="#" title="Edit">Edit</a>
-                                </li>
-                            </ul>
-                        </div>
+                <nav className="aui-navgroup-vertical">
+
+                    <div className="aui-nav-heading" title="bug-page-search">
+                        <strong>Search Bugs In Project</strong>
                     </div>
+
+                    <DropdownSelector id="bug-age" options={timeline_options} title="Age"/>
+                    <DropdownSelector id="bug-status" options={status_options} title="Status"/>
+                    <DropdownSelector id="bug-priority" options={magnitudal_options} title="Priority"/>
+                    <DropdownSelector id="bug-severity" options={magnitudal_options} title="Severity"/>
+
                 </nav>
             </div>
         );
     }
 }
 
+SidebarSearch = reduxForm({
+    form: 'sidebar-search'
+})(SidebarSearch)
 export default SidebarSearch;
