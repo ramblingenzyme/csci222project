6711a7f4de739f3cc91f2493146ae4ca744a0e4a
kranzuft <kranzuft@users.noreply.github.com>
2016-10-10 00:25:24 +1100

    Realised error was occurring for not providing keys to children. Disabled dropdown boxes for now (commented out). Will need help

--- frontend/src/components/SidebarSearch.js
+++ frontend/src/components/SidebarSearch.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Search from './Search.js'
 import { Field, reduxForm } from 'redux-form';
 import DropdownSelector from './DropdownSelector.js'
+import InputField from './InputField.js'
 
 import '../css/App.css';
 
 // This is the default sidebar; for sort bugs
+
+/* TODO: Fix Dropdown Selector Code
+ <DropdownSelector id="bug-age" options={timeline_options} title="Age"/>
+ <DropdownSelector id="bug-status" options={status_options} title="Status"/>
+ <DropdownSelector id="bug-priority" options={magnitudal_options} title="Priority"/>
+ <DropdownSelector id="bug-severity" options={magnitudal_options} title="Severity"/>
+ */
 class SidebarSearch extends Component {
     render() {
         var timeline_options = [
@@ -29,15 +37,9 @@ class SidebarSearch extends Component {
         return (
             <div className="aui-page-panel-nav">
                 <nav className="aui-navgroup-vertical">
-
                     <div className="aui-nav-heading" title="bug-page-search">
                         <strong>Search Bugs In Project</strong>
                     </div>
-
-                    <DropdownSelector id="bug-age" options={timeline_options} title="Age"/>
-                    <DropdownSelector id="bug-status" options={status_options} title="Status"/>
-                    <DropdownSelector id="bug-priority" options={magnitudal_options} title="Priority"/>
-                    <DropdownSelector id="bug-severity" options={magnitudal_options} title="Severity"/>
 
                 </nav>
             </div>
@@ -48,4 +50,5 @@ class SidebarSearch extends Component {
 SidebarSearch = reduxForm({
     form: 'sidebar-search'
 })(SidebarSearch)
+
 export default SidebarSearch;
