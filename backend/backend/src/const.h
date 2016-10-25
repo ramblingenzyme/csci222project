2ae4bca9e2e561a9db866dd0922f7f72104df471
Geniusflip <ryanherford@gmail.com>
2016-10-25 00:20:16 -0700

    added const.h

---
+++ backend/backend/src/const.h
@@ -0,0 +1,7 @@
+#ifndef CONST_H
+#define CONST_H
+#include <string>
+
+const std::string CONNECTION_DETAILS="dbname=bugtracker user=postgres password=csci222password host=csci222-ass2.cy0af74vuu5h.ap-southeast-2.rds.amazonaws.com";
+
+#endif
