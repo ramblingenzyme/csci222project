4c1856270b72ae841d7368d9683eb992f41f24ab
Satvik Sharma <satvik.sharma2@gmail.com>
2016-10-26 17:12:07 +1100

    added profile list actions/reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -7,6 +7,7 @@ import bugList from './bugList';
 import bugPage from './bugPage';
 import profile from './profile';
 import requesting from './requesting';
+import profileList from './profileList';
 import authenticateUser from './authenticateUser';
 
 const user = {
@@ -23,6 +24,7 @@ const baseReducer = combineReducers({
     bugPage,
     profile,
     requesting,
+    profileList,
     authenticateUser,
     form: formReducer,
     forms: combineForms({
