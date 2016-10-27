5721a5ee6a3566d7132cc0ab19ec8cded26c97ad
Andrew Sheldon <asheldon55@gmail.com>
2016-10-27 13:57:12 +1100

    Add project_test to compile script and fix query

--- testing/compile_tests.sh
+++ testing/compile_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -o run_tests -std=c++11 attachment_test.cpp bug_test.cpp comment_test.cpp user_test.cpp main.cpp -lcppunit -lpqxx
+g++ -o run_tests -std=c++11 attachment_test.cpp bug_test.cpp comment_test.cpp project_test.cpp user_test.cpp main.cpp -lcppunit -lpqxx
--- testing/project_test.h
+++ testing/project_test.h
@@ -27,13 +27,6 @@ protected:
     bool string_is_equal(std::string s1, std::string s2);
     project create_test_project();
     bool project_is_equal(project& p1, project& p2);
-    // void test_empty();
-    // void test_find_bug_id();
-    // void test_get_bug_overview();
-    // void test_get_complete_bug_info();
-    // void test_set_bug_info();
-    // void test_update_bug(); //stub
-    //TODO : An implementation of test equals functions could be useful
 };
 
 #endif
